2639ff42831b0032982ae2111d3875140fb78117
Boseok <dyacode@pmh.codes>
2023-12-27 17:12:56 +0900

    feat: frontend mainpage

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <h1 class="logo">
+        <a href="/" />
+      </h1>
+
+      <nav>
+        <ul>
+          <li><a href="">생각하기</a></li>
+          <li><a href="">만들기</a></li>
+          <li><a href="">공유하기</a></li>
+          <li><a href="">커뮤니티</a></li>
+        </ul>
+      </nav>
+
+      <div class="menu_container">
+        <div class="menu">
+          <a class='icon'>
+            <img class='search' src="https://playentry.org/img/IcoTopSearch.svg" alt="">
+          </a>
+
+<!--          IsLogined-->
+<!--          <a class='icon'>-->
+<!--            <img class='notice' src="https://playentry.org/img/IcoTopAlarm.svg" alt="">-->
+<!--          </a>-->
+<!--          <a class='icon' href="/">-->
+<!--            <img class='profile' src="https://playentry.org/uploads/87/4w/874wed1gklsv5s5w003hf51f2bgj6ui2.svg" alt="">-->
+<!--          </a>-->
+
+          <a class="icon" href="">
+            <div class="login">로그인</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+@import "../styles/nav.css";
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <main>
+    <NavBar/>
+
+    <div class="bannerContainer">
+      <div class="banner">
+        <div class="bannerInner">
+
+          <div class="textbox">
+            <strong class="titleStyle">엔트리 10주년 '브랜드 필름'</strong>
+            <p class="subTitleStyle">가슴이 웅장해지는 48초, 지금 확인해 보세요!</p>
+
+            <a class="bannerBtn" href="https://campaign.naver.com/entry-10th-anniversary/">자세히 보기</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="staffContainer">
+      <div class="staff">
+
+      </div>
+    </div>
+
+    <div class="popularContainer">
+      <div class="popularMenu">
+        <h2>인기 작품</h2>
+        <a href="/" class="popularBtn">
+          <span>더 보기</span>
+        </a>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@import "../styles/home.css";
+</style>
