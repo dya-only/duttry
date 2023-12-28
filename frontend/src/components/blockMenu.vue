d6c08a172ef2f8a899d19621476fcb46c631cd6c
Boseok <dyacode@pmh.codes>
2023-12-28 11:04:26 +0900

    feat: blocks

---
+++ frontend/src/components/blockMenu.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps({
+  logo: String,
+  name: String
+})
+
+const setLogo = computed(() => ({
+  backgroundImage: `url(${props.logo})`,
+}))
+</script>
+
+<template>
+  <li :style="setLogo">{{ name }}</li>
+</template>
+
+<style scoped>
+  li {
+    -webkit-font-smoothing: subpixel-antialiased;
+    user-select: none;
+    list-style: none;
+    font-weight: 700;
+    box-sizing: border-box;
+    margin: 0;
+    -webkit-text-size-adjust: none;
+    padding: 34px 0 4px;
+    min-width: 58px;
+    text-align: center;
+    font-size: 12px;
+    color: #595959;
+    border-bottom: 1px solid #d6e9f4;
+    cursor: pointer;
+    font-family: EntryFont,NanumGothic,Nanum Gothic, sans-serif;
+    line-height: 14px;
+    background-size: 32px auto;
+    background-position: 50% 4px;
+    position: relative;
+    background-color: #ecf8ff;
+    z-index: 5;
+    background-repeat: no-repeat;
+  }
+</style>
--- frontend/src/views/ProjectView.vue
+++ frontend/src/views/ProjectView.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
 
+import BlockMenu from "@/components/blockMenu.vue"
+import {ref} from "vue"
+import { VueDraggableNext } from 'vue-draggable-next'
+
+const availableBlocks = ref([
+  {
+    id: 1,
+    name: '시작되었을 때',
+    blockType: 'start',
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: 2,
+    name: '앞으로 10 움직이기',
+    blockType: 'action',
+    blockAction: 'x + 10',
+    position: { x: 100, y: 150 }
+  }
+])
+const usedBlocks = ref([])
 </script>
 
 <template>
@@ -65,7 +85,7 @@
                       <div id="7dwq" class="selectedScene">
                         <span class="entrySceneLeftWorkspace"></span>
                         <span class="entrySceneInputCover">
-                          <input class="entrySceneFieldWorkspace" value="장면 1" />
+                          <input class="entrySceneFieldWorkspace" value="장면 1"/>
                         </span>
                         <span class="entrySceneRemoveButtonCoverWorkspace">
                           <button class="entrySceneRemoveButtonWorkspace"></button>
@@ -87,17 +107,57 @@
     </div>
   </header>
 
-  <div class="playPanel">
-    <button class="speedBtn"></button>
-    <button class="scaleBtn"></button>
-    <button class="gridBtn"></button>
+  <div class="panelContainer">
+    <div class="left">
+      <div class="playPanel">
+        <button class="speedBtn"></button>
+        <button class="scaleBtn"></button>
+        <button class="gridBtn"></button>
 
-    <canvas width="640" height="360"></canvas>
-  </div>
+        <canvas width="640" height="360"></canvas>
+      </div>
 
-  <div class="engineBtnContainer">
-    <button class="objectAddBtn">오브젝트 추가하기</button>
-    <button class="startEngineBtn">시작하기</button>
+      <div class="engineBtnContainer">
+        <button class="objectAddBtn">오브젝트 추가하기</button>
+        <button class="startEngineBtn">시작하기</button>
+      </div>
+
+      <div class="objectListContainer"></div>
+    </div>
+
+    <div class="right">
+      <ul class="tabList">
+        <li class="tabItem">블록</li>
+        <li class="tabItemDisable">모양</li>
+        <li class="tabItemDisable">소리</li>
+        <li class="tabItemDisable">속성</li>
+      </ul>
+
+
+      <div class="workSpace">
+        <ul class="blockMenuContainer">
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/start.svg' name='시작' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/flow.svg' name='흐름' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/moving.svg' name='움직임' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/looks.svg' name='생김새' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/brush.svg' name='붓' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/sound.svg' name='소리' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/judgement.svg' name='판단' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/calc.svg' name='계산' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/variable.svg' name='자료' />
+          <blockMenu logo='https://playentry.org/lib/entry-js/images/func.svg' name='함수' />
+        </ul>
+
+        <div class="blockContainer">
+        </div>
+
+        <div class="workspacePanel">
+          <div class="entryBg">
+<!--            <img v-for="n in 300" :key="n" class="bg" src="https://playentry.org/lib/entry-js/images/entry_bg.svg" alt="">-->
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
